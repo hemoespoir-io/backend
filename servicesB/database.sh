0f77b8972388a6d8516a6887bfaf51a703966e27
Nabil Qasmi <nabil.qasmi1@gmail.com>
2024-07-30 12:57:29 +0200

    feat-5

--- servicesB/database.sh
+++ servicesB/database.sh
@@ -1,38 +1,17 @@
+#connexion avec le root
+mysql -u root -p
+#affichage des base de donnes
+show databases;
 
-Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
-
-mysql> create database pfe;
-Query OK, 1 row affected (0.00 sec)
-
-mysql> exit;
-Bye
-
-C:\Users\cele\hemoespoir.io\backend\servicesB>mysql -u root -p pfe < shema.sql
-Enter password:
-
-C:\Users\cele\hemoespoir.io\backend\servicesB>mysql -u root -p
-Enter password:
-Welcome to the MySQL monitor.  Commands end with ; or \g.
-Your MySQL connection id is 26
-Server version: 5.1.30-community MySQL Community Server (GPL)
-
-Copyright (c) 2000, 2023, Oracle and/or its affiliates.
-
-Oracle is a registered trademark of Oracle Corporation and/or its
-affiliates. Other names may be trademarks of their respective
-owners.
-
-Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
-
-mysql> mysql -u root -p
-    ->
-    -> use pfe;
-
-
-use pfe' at line 1
-mysql> use pfe;
-Database changed
-mysql> shwo databases
-    -> shwo databases;
-
-
+#creation de la base de donnnees 
+CREATE DATABASE pfe ;
+#creation d'uun utilisateur 
+CREATE USER 'hemoespoir'@'$1' IDENTIFIED BY '$2';
+#les droit dacces 
+GRANT ALL ON pfe.* TO 'hemoespoir'@"$1";
+#privillieges
+FLUSH PRIVILEGES;
+#exit mysql server
+exit;
+#importation du fichier sql 
+mysql -u hemoespoir -p pfe < shema.sql
